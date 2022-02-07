e271c343db52db2860d966df8c41a81d7840bfbf
Joshua Whitmore <joshuawhitmore3@gmail.com>
2022-02-06 18:31:20 -0600

    Spring IO Commit

---
+++ src/main/java/com/dogeWallet/doge_wallet/DogeWalletApplication.java
@@ -0,0 +1,13 @@
+package com.dogeWallet.doge_wallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogeWalletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogeWalletApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dogeWallet/doge_wallet/DogeWalletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dogeWallet.doge_wallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogeWalletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
